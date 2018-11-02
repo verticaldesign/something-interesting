a238fe543e1fbb0f048d56556478125e6a4ccb28
erictuftee <erictuftee@gmail.com>
2018-11-02 16:25:24 -0500

    feat: add route links

--- src/components/common/NavHeader/NavHeader.js
+++ src/components/common/NavHeader/NavHeader.js
@@ -1,7 +1,15 @@
 import React from "react";
+import { Link } from "@reach/router";
+import "./nav-styles.css";
 
 const NavHeader = () => {
-  return <nav className="nav-header">Hello</nav>;
+  return (
+    <nav className="nav-header">
+      <Link to="/">Home</Link>
+      <Link to="giphy">Giphy</Link>
+      <Link to="git-repos">Git Repos</Link>
+    </nav>
+  );
 };
 
 export default NavHeader;
