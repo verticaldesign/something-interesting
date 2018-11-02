7ba99c9cacc2e88f024928595f7933086f99bc55
Eric Tuftee <erictuftee@gmail.com>
2018-11-02 17:15:59 -0500

    feat: search form add to giphy

--- src/components/Giphy/Giphy.js
+++ src/components/Giphy/Giphy.js
@@ -1,15 +1,20 @@
 import React from "react";
-import PropTypes from "prop-types";
+// import PropTypes from "prop-types";
 import { fetchFromGiphy } from "../../actions/gifs";
 import { connect } from "react-redux";
+import SearchTerm from '../common/SearchTerm/SearchTerm'
 
 export class Giphy extends React.Component {
-  componentDidMount() {
-    this.props.fetchFromGiphy("dog");
+  // componentDidMount() {
+  //   this.props.fetchFromGiphy("dog");
+  // }
+  handleSubmit=(term)=>{
+    this.props.fetchFromGiphy(term)
   }
   render() {
     return (
       <section className="giphy">
+      <SearchTerm handleSubmit={this.handleSubmit}></SearchTerm>
         <ul className="giphy-list">{renderList(this.props.giphyItems)}</ul>
       </section>
     );
@@ -17,7 +22,11 @@ export class Giphy extends React.Component {
 }
 
 function renderList(gifs) {
+  if(gifs.length=== 0){
+    return <span className='no-result'>Nothing to see here</span>
+  }else{
   return gifs.map(gif => <GiphyItem key={gif.id} gif={gif} />);
+  }
 }
 
 const GiphyItem = ({ gif }) => {
