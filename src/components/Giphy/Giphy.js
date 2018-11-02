cf9a925f9cc8f0b4cab66d0a22901eedd56b8310
erictuftee <erictuftee@gmail.com>
2018-11-02 09:34:26 -0500

    fix: prop is not a function

--- src/components/Giphy/Giphy.js
+++ src/components/Giphy/Giphy.js
@@ -5,7 +5,6 @@ import { connect } from "react-redux";
 
 export class Giphy extends React.Component {
   componentDidMount() {
-    console.log({ props: this.props });
     this.props.fetchFromGiphy("dog");
   }
   render() {
